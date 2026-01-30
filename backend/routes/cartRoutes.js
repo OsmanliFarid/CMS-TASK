f37c2ec331bba81878bb23ba9676647c6df5d72c
OsmanliFarid <osmanliferid686@gmail.com>
2026-01-30 23:10:13 +0400

    changs text

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const cartRoutes = express.Router();
 cartRoutes.get("/", (req, res) => {
-  res.json({ message: "Cart route works!" });
+  res.json({ message: "Cart route works" });
 });
 export default cartRoutes;
