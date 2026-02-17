e97b3e5932bbcfd5e2c97988cddc3e6a1b7cb5f1
OsmanliFarid <osmanliferid686@gmail.com>
2026-02-17 11:04:29 +0400

    backend code changed

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -13,6 +13,33 @@ cartRoutes.get("/", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+// get cart statistics
+cartRoutes.get("/statistics", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM cartlist");
+
+    const values = result.rows.map((row) => Number(row.value.split(".")[0]));
+
+    const Wons = result.rows.filter((r) => r.status === "Won").length;
+
+    const total = result.rowCount;
+
+    const conversionRate = total === 0 ? 0 : (Wons / total) * 100;
+
+    const statistics = {
+      total: total,
+      sum: values.reduce((acc, val) => acc + val, 0),
+      won: Wons,
+      conversionRate: conversionRate.toFixed(2) + "%",
+    };
+
+    res.json(statistics);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 // get filter status cart items
 cartRoutes.get("/filter", async (req, res) => {
   const { status } = req.query;
